04699630537c3fe7da926307d8e293a2ecefdc70
presys <presys00@gmail.com>
2019-02-13 00:50:51 +0900

    [update]Hello! webスクレイパー
    依存ライブラリはrequirements.txtに記載するように指定

--- src/hello.py
+++ src/hello.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
-
-def hello(name):
-    print("Hello World" + name)
+import requests
+from bs4 import BeautifulSoup
 
 def main():
-    hello(" of Python3!")
+    #--- 取得URLの決定
+    url = 'https://github.com/librariapj'
+    #--- requestsでdom取得
+    response = requests.get(url)
+    #--- 取得結果の表示
+    print(response)
+    # print(response.text)
+    #--- BS4に取得内容を格納
+    soup = BeautifulSoup(response.content, 'html.parser')
+    #--- BS4の出力
+    # 全文
+    #print(soup.prettify())
+    # aタグ
+    print(soup.find_all("a"))
 
 if __name__ == "__main__":
     main()
